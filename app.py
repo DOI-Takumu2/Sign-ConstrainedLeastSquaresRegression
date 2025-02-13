d09dc51c31c4b3f109dfed1da713c2b060067aca
DOI-Takumu2 <takumu_doi@pinegrace.work>
2025-02-13 09:20:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,12 +26,15 @@ st.markdown(
 st.markdown(
     """
     <div style="text-align: right; font-size: 12px; color: gray; margin-top: 20px;">
-        本ツールを使用する際は、以下のように引用を記載していただけると嬉しいです。：<br>
-        <b>DOI, Takumu（2025）SignReg [Computer software]. 使用日: YYYY-MM-DD.</b>
+        本ツールを使用する際は、以下の例のように引用を記載していただけると嬉しいです。<br>
+        <i>If you use this tool, we would appreciate it if you could cite it as follows:</i><br><br>
+        <b>DOI, Takumu (2025). SignReg [Computer software]. Usage date: YYYY-MM-DD.</b>
     </div>
     """,
     unsafe_allow_html=True
-)
+)st.markdown(
+    """
+    
 # **スペース（余白）を追加**
 st.markdown("<br><br>", unsafe_allow_html=True)
 # **ファイルアップロード**
